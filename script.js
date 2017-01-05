c3c21b1b307b7f97e9f90e7db7a00948b982d93c
avoytovich <levanwork@gmail.com>
2017-01-05 07:30:24 +0000

    Initial commit

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 $(document).ready(function () {
    $('.content_one').click(content_one);
    $('.content_two').click(content_two);
-   $('.content_tree').click(content_tree);
+   $('.content_three').click(content_three);
 
 
 
 function content_one () {
-    $('#content_two, #content_tree').fadeOut(500);
+    $('#content_two, #content_three').fadeOut(500);
     $('#content_one').fadeIn(500);
 }
 
 function content_two () {
-    $('#content_one, #content_tree').fadeOut(500);
+    $('#content_one, #content_three').fadeOut(500);
     $('#content_two').fadeIn(500);
 }
 
-function content_tree () {
+function content_three () {
     $('#content_one, #content_two').fadeOut(500);
-    $('#content_tree').fadeIn(500);
+    $('#content_three').fadeIn(500);
 }
 
 });
